47a7070f15d8df02f4f042388e7b1537d32c26b9
thanhvt <thanhvt@vissoft.vn>
2022-03-01 21:58:49 +0700

    add graph algorithm ex 2

--- graph/inside-circle/inside-circle.c
+++ graph/inside-circle/inside-circle.c
@@ -1,11 +1,34 @@
 #include <common-array.h>
+#include <math.h>
+#include <stdio.h>
+#include <common-utility.h>
 
 int insideCircle(arr_int point, arr_int center, int radius);
 
 void insideCircleDemo() {
-
+    printf("Enter circle radius:\n");
+    int radius = scanInt();
+    printf("Enter x, y dimension of the point:\n");
+    arr_int* xyPoint = scanIntArr(2);
+    printf("Enter x, y dimension of the circle center:\n");
+    arr_int* xyCenter = scanIntArr(2);
+    int isInside = insideCircle(*xyPoint, *xyCenter, radius);
+    if (isInside) {
+        printf("The point is inside the given circle\n");
+    } else {
+        printf("The point is outside the given circle\n");
+    }
+    freeIntArr(xyPoint);
+    freeIntArr(xyCenter);
 }
 
 int insideCircle(arr_int point, arr_int center, int radius) {
-    return 0;
+    int sqr_dist = pow(center.arr[0] - point.arr[0], 2) + pow(center.arr[1] - point.arr[1], 2);
+    // binh phuong dist(A, C)
+    int powRadius = pow(radius, 2);
+    if (sqr_dist <= powRadius) {
+        return 1; // trong hinh tron
+    } else {
+        return 0; // ngoai hinh tron
+    }
 }
