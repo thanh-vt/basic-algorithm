0c0379b53366bb3bdf156d7bc57877bfdf636899
thanhvt <thanhvt@vissoft.vn>
2022-03-17 00:21:53 +0700

    update CMakelists for windows

--- src/geometry/is-rectangle/is-rectangle.c
+++ src/geometry/is-rectangle/is-rectangle.c
@@ -7,5 +7,27 @@ void isRectangleDemo() {
 }
 
 int isRectangle(matrix_int points) {
+//    List<Double> lst = new ArrayList<>();
+//    lst.add(distanceTwoPoin(points[0][0], points[0][1], points[1][0], points[1][1]));
+//    lst.add(distanceTwoPoin(points[2][0], points[2][1], points[3][0], points[3][1]));
+//    lst.add(distanceTwoPoin(points[3][0], points[3][1], points[0][0], points[0][1]));
+//    lst.add(distanceTwoPoin(points[1][0], points[1][1], points[2][0], points[2][1]));
+//    lst.add(distanceTwoPoin(points[0][0], points[0][1], points[2][0], points[2][1]));
+//    lst.add(Math.sqrt(((Math.pow(lst.get(0), 2) + Math.pow(lst.get(2), 2)))));
+//
+//    for (int i = 0; i < lst.size(); i = i + 2) {
+//        if (Double.compare(lst.get(i), lst.get(i + 1)) != 0) {
+//            return false;
+//        }
+//    }
+//
+//    return true;
+    return 0;
+}
+
+double distanceTwoPoin(int xpoint1, int ypoint1, int xpoint2, int ypoint2) {
+//    double height = Math.abs(xpoint1 - xpoint2);
+//    double weight = Math.abs(ypoint1 - ypoint2);
+//    return Math.sqrt(((Math.pow(height, 2) + Math.pow(weight, 2))));
     return 0;
 }
